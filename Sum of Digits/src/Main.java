92dace650e697a618da6db53325be36ed0828722
Markus Agersten <markus.agersten@gmail.com>
2025-02-21 09:08:44 +0100

    html and css exercise.

---
+++ Sum of Digits/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class main {
+    public static int ain(String[] args) {
+
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int number : numbers) {
+            System.out.println("Element: " + number);
+        }
+    }
+}
---
+++ array traning/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+
+
+            /* array traning
+
+             */
+
+
+
+
+    {
+        System.out.println("Hello, World!");
+    }
+}
