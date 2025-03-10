0482e033be3fc5ea9de8b19d818530ca207c390a
Markus Agersten <markus.agersten@gmail.com>
2025-03-10 11:24:05 +0100

    new calculator switches.

--- Calculator app/src/main/java/Markus/org/Main.java
+++ Calculator app/src/main/java/Markus/org/Main.java
@@ -7,69 +7,50 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
-   
-
-        while (true) {
-
-            System.out.println("Choose an operation: +, -, *, / or type 'exit' to quit the program. \nIf you want to separate decimal numbers. Use a comma (,)");
-            String whatMath = input.next();
-
-            if (whatMath.equals("exit")) {
-                System.out.println("program closes.");
-                break;
-
-            } else if (whatMath.equals("+")) {
-
-                System.out.println("Write a Number You want to add ");
-                double a = input.nextDouble();
-                System.out.println("Write a Number second You want to add ");
-                double b = input.nextDouble();
-                double sum = a + b;
-                System.out.println("Total sum are: " + sum);
-
-            } else if (whatMath.equals("-")) {
-
-                System.out.println("Write a Number You want to add ");
-                double a = input.nextDouble();
-                System.out.println("Write a Number second You want to add ");
-                double b = input.nextDouble();
-                double sum = a - b;
-                System.out.println("Total sum are: " + sum);
-
-            } else if (whatMath.equals("*")) {
-
-                System.out.println("Write a Number You want to add ");
-                double a = input.nextDouble();
-                System.out.println("Write a Number second You want to add ");
-                double b = input.nextDouble();
-                double sum = a * b;
-                System.out.println("Total sum are: " + sum);
-
-            } else if (whatMath.equals("/")) {
+        boolean validOperation = true;
+        while (validOperation) {
+            double num1;
+            double num2;
+            char operator;
+            double result = 0;
 
 
-                System.out.println("Write a Number You want to add ");
-                double a = input.nextDouble();
-                System.out.println("Write a Number second You want to add ");
-                double b = input.nextDouble();
-                if (b == 0) {
-                    System.out.println("Cannot divide by zero..");
-                }
-                if (a == 0) {
-                    System.out.println("Cannot divide by zero. Please Chose one of the operations below");
-                } else {
-                    double sum = a / b;
-                    System.out.println("Total sum are: " + sum);
+            System.out.println("Enter the first number");
+            num1 = input.nextDouble();
+            System.out.println("Enter an operator (+, -, *, / ):");
+            operator = input.next().charAt(0);
+
+
+            System.out.println("Enter the second number");
+            num2 = input.nextDouble();
+
+            switch (operator) {
+                case '+' -> result = num1 + num2;
+                case '-' -> result = num1 - num2;
+                case '*' -> result = num1 * num2;
+                case '/' -> {
+                    if (num2 == 0) {
+                        System.out.println("you cant / with 0");
+                        validOperation = false;
+                    } else {
+                        result = num1 / num2;
+
+                    }
 
                 }
-
-            } else {
-                System.out.println("please assign an operation: +, -, *, / or type 'exit' to quit the program.");
+                default -> {
+                    System.out.println("Invalid operation");
+                    validOperation = false;
+                }
             }
+            if (validOperation) {
+                System.out.println(result);
+            }
+
         }
-
-
+        input.close();
     }
+
 }
+
 
