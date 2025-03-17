ed70eebb1cf2cef348a0fb17f44a85ebadc72008
Markus Agersten <markus.agersten@gmail.com>
2025-03-17 11:42:59 +0100

    Added try catch for error handeling

--- Workshop Currency Converter App/src/main/java/Markus/org/CurrencyConverterStart.java
+++ Workshop Currency Converter App/src/main/java/Markus/org/CurrencyConverterStart.java
@@ -1,6 +1,7 @@
 package Markus.org;
 
 import java.time.LocalDate;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CurrencyConverterStart {
@@ -19,33 +20,66 @@ public class CurrencyConverterStart {
             switch (choice) {
 
                 case "1":
+
                     System.out.println(LocalDate.now() + " SEK to dollars Currency rate: " + currencyExchanger.SekRate);
                     System.out.print("Enter the amount of SEK to USD: ");
                     System.out.println(" Current currency rate: " + currencyExchanger.SekRate);
-                    double sekToUs = currencyExchanger.sekToUsd(input.nextDouble());
-                    System.out.println(LocalDate.now() + " Amount: " + sekToUs + currencyExchanger.getDollarSymbol);
+                    try {
+                        double sekToUs = currencyExchanger.sekToUsd(input.nextDouble());
+                        System.out.println(LocalDate.now() + " Amount: " + sekToUs + currencyExchanger.getDollarSymbol);
+                        break;
 
-                    break;
+
+                    } catch (InputMismatchException e) {
+                        System.out.println("Error: Please enter a valid number! Use (,) as a separator");
+                        input.nextLine();
+                        break;
+                    }
+
 
                 case "2":
                     System.out.println(LocalDate.now() + " Dollars to SEK Currency rate: " + currencyExchanger.usdRate);
                     System.out.print("Enter the amount of USD to SEK: ");
-                    double usdToSe = currencyExchanger.usdToSek(input.nextDouble());
-                    System.out.println(LocalDate.now() + " Amount: " + usdToSe + currencyExchanger.getSekSymbol);
+                    try {
+                        double usdToSe = currencyExchanger.usdToSek(input.nextDouble());
+                        System.out.println(LocalDate.now() + " Amount: " + usdToSe + currencyExchanger.getSekSymbol);
+                        break;
 
-                    break;
+
+                    } catch (InputMismatchException e) {
+                        System.out.println("Error: Please enter a valid number! Use (,) as a separator");
+                        input.nextLine();
+                        break;
+                    }
+
+
                 case "3":
                     System.out.println(LocalDate.now() + " SEK to Euro Currency rate: " + currencyExchanger.euroRate);
                     System.out.print("Enter the amount of SEK to EURO: ");
-                    double sekToEu = currencyExchanger.sekToEuro(input.nextDouble());
-                    System.out.println(LocalDate.now() + " Amount: " + sekToEu + currencyExchanger.getEuroSymbol);
-                    break;
+                    try {
+                        double sekToEu = currencyExchanger.sekToEuro(input.nextDouble());
+                        System.out.println(LocalDate.now() + " Amount: " + sekToEu + currencyExchanger.getEuroSymbol);
+                        break;
+
+                    } catch (InputMismatchException e) {
+                        System.out.println("Error: Please enter a valid number! Use (,) as a separator");
+                        input.nextLine();
+                        break;
+                    }
+
+
                 case "4":
                     System.out.println(LocalDate.now() + " | Euro to SEK Currency rate: " + currencyExchanger.SekERate);
                     System.out.print("Enter the amount of EURO to SEK: ");
-                    double euroToSe = currencyExchanger.euroToSek(input.nextDouble());
-                    System.out.println(LocalDate.now() + " Amount: " + euroToSe + currencyExchanger.getSekSymbol);
-                    break;
+                    try {
+                        double euroToSe = currencyExchanger.euroToSek(input.nextDouble());
+                        System.out.println(LocalDate.now() + " Amount: " + euroToSe + currencyExchanger.getSekSymbol);
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.out.println("Error: Please enter a valid number! Use (,) as a separator");
+                        input.nextLine();
+                        break;
+                    }
                 case "0":
                     isRunning = false;
                     System.out.println("Closing Program...");
@@ -53,9 +87,13 @@ public class CurrencyConverterStart {
                 default:
                     System.out.println("Please insert a number between 0-4 or enter 0 to exit.");
             }
+
+
         }
         input.close();
 
 
     }
+
+
 }
