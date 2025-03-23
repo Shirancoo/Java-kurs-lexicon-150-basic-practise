ef83d05af706efaae0d88243480c5f4be42ff2e6
Markus Agersten <markus.agersten@gmail.com>
2025-03-23 21:13:11 +0100

    added new array work shop

---
+++ G55-Array-Workshop-main/G55-Array-Workshop-main/src/main/java/se/lexicon/App.java
@@ -0,0 +1,16 @@
+package se.lexicon;
+
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+
+        int size = NameRepository.getSize();
+        System.out.println(size);
+        NameRepository.setNames(new String[]{"Erik Svensson", "Mehrdad Javan"});
+        System.out.println(NameRepository.getSize());
+        System.out.println(Arrays.toString(NameRepository.setNames(names)));
+        // call more methods as needed
+    }
+}
