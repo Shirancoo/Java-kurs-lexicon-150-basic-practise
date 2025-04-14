8d3be5a0095278c4d4c22ed025225e33f99b0c8a
Markus Agersten <markus.agersten@gmail.com>
2025-04-14 10:51:43 +0200

    addes new projects

--- Studentregister/src/main/java/org/example/Programs.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Programs implements Displayable{
-
-    @Override
-    public void displayinfo() {
-        System.out.println(StudyProgram.SO + "\n" + StudyProgram.MATH + "\n" + StudyProgram.ECONOMICS);
-    }
-}
